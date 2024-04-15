2e2463af283a87c9fe033663f217534493792373
Victor Santos <victords895@gmail.com>
2024-04-15 15:42:41 -0300

    middleware refactor

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,15 @@ import { NextResponse, type NextRequest } from 'next/server'
 export function middleware(request: NextRequest): NextResponse | undefined {
   const userToken = request.cookies.get('userToken')?.value
   const userTokenExists = userToken !== undefined
+  const publicRoutes = ['/', '/account/new']
 
-  if (userTokenExists && ['/', '/account/new'].includes(request.nextUrl.pathname)) {
+  if (userTokenExists && publicRoutes.includes(request.nextUrl.pathname)) {
     const url = request.nextUrl.clone()
     url.pathname = '/menu'
     return NextResponse.redirect(url)
   }
 
-  if (!userTokenExists && !['/', '/account/new'].includes(request.nextUrl.pathname)) {
+  if (!userTokenExists && !publicRoutes.includes(request.nextUrl.pathname)) {
     const url = request.nextUrl.clone()
     url.pathname = '/'
     return NextResponse.redirect(url)
