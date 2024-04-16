79b318ae580a40718a772ed34ed6197faef0f909
Victor Santos <victords895@gmail.com>
2024-04-15 21:18:11 -0300

    fix api baseUrl

--- src/modules/axios.ts
+++ src/modules/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'https://gluco-care-api.azurewebsites.net' : ''
+  baseURL: 'https://gluco-care-api.azurewebsites.net'
 })
 
 export default axiosInstance
